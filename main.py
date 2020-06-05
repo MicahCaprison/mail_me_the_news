132b1e6f538752c66b599d6c2d387e193e55f20f
MicahCaprison <micahdiggs@gmail.com>
2020-06-05 10:01:48 -0400

    F created four basic math functions for use in future calculator

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("Micah's first python program!!!!")
+
+def add_two_variables(variable_one, variable_two):
+    return variable_one + variable_two
+
+def subtract_two_variables(variable_one, variable_two):
+    return variable_one - variable_two
+    
+def multiply_two_variables(variable_one, variable_two):
+    return variable_one * variable_two
+
+def divide_two_variables(variable_one, variable_two):
+    return variable_one / variable_two
+
+print( add_two_variables(25, 75) )
+
+print( subtract_two_variables(50, 20) )
+
+print( multiply_two_variables(5, 5) )
+
+print( divide_two_variables(50.50, 2) )
